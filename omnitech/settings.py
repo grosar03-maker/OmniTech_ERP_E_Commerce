f11c109142cea61d3df09592687decd13afc3058
Gustavo Rosa <grosar03@gmail.com>
2026-04-26 20:52:24 -0400

    Fix ALLOWED_HOSTS for Render

--- omnitech/settings.py
+++ omnitech/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-omnitech-dev-key-chan
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.auth',
