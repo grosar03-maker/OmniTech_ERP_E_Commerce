d090cf98ebbf0b8cb56689ea2be1f7e60ce3e00d
Gustavo Rosa <grosar03@gmail.com>
2026-04-26 21:02:57 -0400

    Add whitenoise for static files

--- omnitech/wsgi.py
+++ omnitech/wsgi.py
@@ -4,7 +4,9 @@ WSGI config for OmniTech project.
 
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omnitech.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/opt/render/project/repo/staticfiles')
