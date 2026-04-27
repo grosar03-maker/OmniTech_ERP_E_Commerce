0720666627b83f80e5da7d4e1967fbbcc64e229a
Gustavo Rosa <grosar03@gmail.com>
2026-04-26 21:16:56 -0400

    Add release phase for migrate

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate --noinput
 web: python manage.py collectstatic --noinput && gunicorn omnitech.wsgi --log-file -
--- omnitech/settings.py
+++ omnitech/settings.py
@@ -80,7 +80,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
