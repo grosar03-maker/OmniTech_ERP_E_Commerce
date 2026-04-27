a3a6de5f7ec14f53e718831da7c3484e054d6e07
Gustavo Rosa <grosar03@gmail.com>
2026-04-26 21:12:06 -0400

    Simplify wsgi, add whitenoise only in production

--- omnitech/settings.py
+++ omnitech/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -35,6 +34,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if not DEBUG:
+    MIDDLEWARE.insert(0, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 ROOT_URLCONF = 'omnitech.urls'
 
@@ -79,9 +81,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Whitenoise storage
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- omnitech/wsgi.py
+++ omnitech/wsgi.py
@@ -4,15 +4,7 @@ WSGI config for OmniTech project.
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omnitech.settings')
 
 application = get_wsgi_application()
-
-try:
-    base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    static_root = os.path.join(base, 'staticfiles')
-    application = WhiteNoise(application, root=static_root, prefix='static/')
-except Exception:
-    pass
