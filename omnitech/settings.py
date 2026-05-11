de1e97049883e336ae0a0a1abd72e799f90c5cb3
Gustavo Rosa <grosar03@gmail.com>
2026-05-11 14:23:17 -0400

    Fix: add EMAIL_TIMEOUT=5 to prevent SMTP hang on pago_exitoso

--- omnitech/settings.py
+++ omnitech/settings.py
@@ -111,6 +111,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'grosar2006@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_TIMEOUT = 5
 EMAIL_FROM = 'OmniTech <noreply@omnitech.cl>'
 DEFAULT_FROM_EMAIL = 'OmniTech <noreply@omnitech.cl>'
 
