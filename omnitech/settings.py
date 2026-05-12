ffebff8838c6d63b296a552af5318480a7da52ba
Gustavo Rosa <grosar03@gmail.com>
2026-05-12 08:12:16 -0400

    Fix: change SMTP to port 465 SSL since Render blocks port 587

--- omnitech/settings.py
+++ omnitech/settings.py
@@ -107,8 +107,8 @@ RESERVA_TIMEOUT_MINUTES = 15
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'grosar2006@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_TIMEOUT = 5
